f3480f6526d74929aae7c0892f2d9cd09708e34f
Mana <andriamamonjylaza@gmail.com>
2017-05-12 17:23:57 +0300

    Create template-loop.php

---
+++ booking/template-loop.php
@@ -0,0 +1,48 @@
+<section class="container-wrap  main-color ">
+	<div class="section-container container">
+		<div class="vc_row vc_row-fluid row">
+			<div class="wpb_column vc_column_container vc_col-sm-12">
+				<div class="vc_column-inner ">
+					<div class="wpb_wrapper">
+						<div class="vc_row wpb_row vc_inner vc_row-fluid vc_column-gap-10 vc_row-o-equal-height vc_row-o-content-top vc_row-flex">
+<!-- if query->have_posts() -->
+							<!-- start loop -->
+							<!-- while query->have->posts() -->
+							<div class="wpb_column vc_column_container vc_col-sm-4">
+								<div class="vc_column-inner ">
+									<div class="wpb_wrapper">
+										<div class="kleo_text_column wpb_content_element  luxueuses-villas vc_custom_1494420149540">
+											<div class="wpb_wrapper">
+												<p>
+													<a href="http://www.home-la-residence.com/new-site/location-vacances-nosybe-villa-tongasoa/"><br>
+														<img class="villas-location" src="http://www.home-la-residence.com/new-site/wp-content/uploads/2014/03/villa-tongasoa-nosy-be.jpg" alt="Villa Tongasoa" width="700" height="400"><br>
+														<span class="bg-transparent">
+															<span class="partir">à partir de</span>
+															<span class="prix">163<span class="euro">€</span></span>
+														</span>
+													</a><br>
+													<span class="description-villas"><span class="titre">VILLA TONGASOA</span><br>
+														<span class="description">Une villa de luxe les pieds dans l’eau pour 2 à 6 personnes.</span>
+													</span><br>
+													<span class="reservation"><a style="color: #333333;" href="http://www.home-la-residence.com/new-site/reservation/"> <i class="fa fa-phone"></i> Réservation</a></span><span class="détails"><a style="color: #333333;" href="http://www.home-la-residence.com/new-site/location-vacances-nosybe-villa-tongasoa/"><i class="fa fa-search-plus"></i> Voir détails</a></span>
+												</p>
+
+											</div> 
+										</div> 
+									</div>
+								</div>
+							</div>
+							<!-- if reste division par 3 = 0 -->
+							<!-- <div class="vc_empty_space" style="height: 30px"><span class="vc_empty_space_inner"></span></div> -->
+							<!-- if reste division par 3 = 0 -->
+							<!-- wp_reset_post() -->
+							<!-- end query->have->posts() -->
+							<!-- end loop -->
+<!-- end if query->have_posts() -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>							
